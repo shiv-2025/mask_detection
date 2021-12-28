92e583ed946b11bc682216c2bb576a07c8c263af
Shiv Suthar <82932314+shiv-2025@users.noreply.github.com>
2021-12-28 19:08:13 +0530

    Update mask-app.py

--- mask-app.py
+++ mask-app.py
@@ -86,38 +86,7 @@ def main():
         else:
             st.text("Please upload an image file")
 
-    elif choice == "Face Verification":
-        column1, column2 = st.columns(2)
     
-        with column1:
-            image1 = st.file_uploader("Choose File", type=["jpg","png", "jpeg"])
-           
-        with column2:
-            image2 = st.file_uploader("Select File", type=["jpg","png", "jpeg"])
-        if (image1 is not None) & (image2  is not None):
-            col1, col2 = st.columns(2)
-            image1 =  Image.open(image1)
-            image2 =  Image.open(image2)
-            with col1:
-                st.image(image1)
-            with col2:
-                st.image(image2)
-
-            filenames = [image1,image2]
-
-            faces = [extract_face(f) for f in filenames]
-            samples = np.asarray(faces, "float32")
-            samples = preprocess_input(samples, version=2)
-            model2 = VGGFace(model= "resnet50" , include_top=False, input_shape=(224, 224, 3),
-            pooling= "avg" )
-            # perform prediction
-            embeddings = model2.predict(samples)
-            thresh = 0.5
-            score = cosine(embeddings[0], embeddings[1])
-            if score <= thresh:
-                st.success( " >face is a match (%.3f <= %.3f) " % (score, thresh))
-            else:
-                st.error(" >face is NOT a match (%.3f > %.3f)" % (score, thresh))
 
 def extract_face(file):
     pixels = np.asarray(file)
